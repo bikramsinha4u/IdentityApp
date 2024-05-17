9aa3502a1deae8179fce2e575fd77c0c95d895de
Bikram Kumar Sinha <bikramsinha4u@yahoo.com>
2024-05-17 16:11:33 +0530

    basic link for all routes

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -3,24 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
 import { AccountModule } from './account/account.module';
+import { PlayComponent } from './play/play.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
-    path: 'account', loadChildren: () => import('./account/account.module').then(module => module.AccountModule)
+    path: 'play',
+    component: PlayComponent,
   },
   {
-    path: 'not-found', component: NotFoundComponent
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((module) => module.AccountModule),
   },
   {
-    path: '**', component: NotFoundComponent, pathMatch: "full"
-  }
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    pathMatch: 'full',
+  }  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,17 +8,21 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     TooltipModule
   ],
   providers: [],
